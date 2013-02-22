77dc3b999457925f62c70f524dae2c148d5bfaa3
Joe Madden <joe@beardbro.com>
2013-02-22 13:29:56 -0500

    starting this shit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+var stupidCardGame = function () {
 
+    var cards = {};
+
+    function newCard () {
+        var card = Math.floor(Math.random() * 52);
+        return card;
+    };
+
+    function generateCards () {
+        var cardNums = [];
+
+        for ( var i = 0, cardsLeft = 12; i < cardsLeft; i++ ) {
+            var newCard = ;
+
+            for ( i in cardNums ) {
+
+            }
+
+        }
+
+    };
+
+};
